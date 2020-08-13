2f5f80a31d21c57ef5f7741605f2e819a72c39f6
amit <amit@DESKTOP-SIU2A5U.mshome.net>
2020-08-13 12:18:10 +0530

    by amit

--- Git Project/src/B.java
+++ Git Project/src/B.java
@@ -6,5 +6,12 @@ public static void main(String[] args) {
 	System.out.println("B class ");
 	
 	System.out.println("B class Change through git process");
+	
+	
+}
+
+public void sum()
+{
+	System.out.println("Sum");
 }
 }
